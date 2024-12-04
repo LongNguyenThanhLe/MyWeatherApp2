6c3f7518d5cfe4daa3c9680edec48c8a1ca0d1b6
Long Le <tlenguye@emich.edu>
2024-12-04 16:59:16 -0500

    update weather_display.py

---
+++ weather_display.py
@@ -0,0 +1,30 @@
+class WeatherDisplay:
+    @staticmethod
+    def format_temperature(temp_k):
+        temp_f = (temp_k * 9/5) - 459.67
+        return f"{temp_f:.0f}°F"
+
+    @staticmethod
+    def get_weather_emoji(weather_id):
+        if 200 <= weather_id <= 232:
+            return "⛈"
+        elif 300 <= weather_id <= 321:
+            return "🌦"
+        elif 500 <= weather_id <= 531:
+            return "🌧"
+        elif 600 <= weather_id <= 622:
+            return "❄"
+        elif 701 <= weather_id <= 741:
+            return "🌫"
+        elif weather_id == 762:
+            return "🌋"
+        elif weather_id == 771:
+            return "💨"
+        elif weather_id == 781:
+            return "🌪"
+        elif weather_id == 800:
+            return "☀"
+        elif 801 <= weather_id <= 804:
+            return "☁"
+        else:
+            return ""
