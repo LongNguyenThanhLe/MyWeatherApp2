15a60b16710ac6cd863e9759a936b0d8b6362766
Long Le <tlenguye@emich.edu>
2025-01-30 02:58:38 -0500

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QApplication
 from weather_ui import WeatherUI
 
 if __name__ == "__main__":
-    API_KEY = "YOUR API KEY HERE"
+    API_KEY = "e9e55e1faf5debbff2273e9422ff5651"
 
     app = QApplication(sys.argv)
     weather_app = WeatherUI(api_key=API_KEY)
